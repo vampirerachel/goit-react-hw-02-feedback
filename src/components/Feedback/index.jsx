f1f7e46429103c60cccdd7fcb140eb97385f56c1
Rachel Frederickson <vampirerachel@gmail.com>
2023-05-13 19:37:29 -0400

    draft 1

--- src/components/Feedback/index.jsx
+++ src/components/Feedback/index.jsx
@@ -4,16 +4,27 @@ import styles from "./style.module.css";
 export default function Feedback() {
  const [feedback, setFeedback] = useState({ Good: 0, Bad: 0, Neutral: 0 });
 
-  function countTotalFeedback() {}
+  function countTotalFeedback() {
+    return feedback.Good + feedback.Bad + feedback.Neutral
+  }
 
-  function countPositiveFeedbackPercentage() {}
+function countPositiveFeedbackPercentage() {
+  const total = feedback.Good + feedback.Neutral + feedback.Bad;
+  const positive = feedback.Good;
+  return total === 0 ? 0 : Math.round((positive / total) * 100);
+}
 
-  function handleChange(event) {};
-  
+  function handleChange(event) {
+    setFeedback(previous => {
+      return { ...previous, [event.target.textContent]: previous[event.target.textContent] + 1, };
+    })
+  };
+
 
   return (
-    <div>
-      <h1 className={styles.title}>Statistics</h1>
+    <div className={styles.buttonRow}>
+      <h1 className={styles.title}>Please Leave Feedback</h1>
+      <div>
       <button className={styles.buttons} onClick={handleChange}>
         Good
       </button>
@@ -22,7 +33,19 @@ export default function Feedback() {
       </button>
       <button className={styles.buttons} onClick={handleChange}>
         Bad
-      </button>
+        </button>
+        </div>
+      
+      <h2>Statistics</h2>
+      {countTotalFeedback() > 0 ? (<div>
+      <h3>Good : {feedback.Good}</h3>
+      <h3>Bad : { feedback.Bad}</h3>
+      <h3>Neutral : {feedback.Neutral}</h3>
+        <h3>Total : {countTotalFeedback()}</h3>
+        <h3> positive feedback : {countPositiveFeedbackPercentage() }%</h3>
+        </div>) : <h3> No feedback Given</h3>}
     </div>
   );
+
+  
 }
